3a6f46f59d99fdc84b99e080fd5514a87c751ade
Kitsu <mail@kitsu.me>
2018-09-24 16:11:20 +0300

    Add loader
    Signed-off-by: Kitsu <mail@kitsu.me>

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@ OBJ = common.o net.o image.o crypto.o flag.o img_db.o
 CFLAGS = -std=c11 -fno-PIC -m32 -O2 -Wall -Wextra
 LIB = -lsqlite3 -lpthread
 AC_PATH = abob_cloud_service
+LD_PATH = loader
 
-all: $(patsubst %,$(OUTDIR)/%,$(OBJ)) $(OUTDIR)/sha256.o $(OUTDIR)/upng.o
+service: $(patsubst %,$(OUTDIR)/%,$(OBJ)) $(OUTDIR)/sha256.o $(OUTDIR)/upng.o
 	$(CC) $(AC_PATH)/main.c $^ $(CFLAGS) $(LIB) -o$(OUTDIR)/abob_cloud
 
 $(OUTDIR)/%.o: $(AC_PATH)/%.c $(AC_PATH)/%.h
@@ -16,6 +17,23 @@ $(OUTDIR)/sha256.o: $(AC_PATH)/SHA256/sha256.c $(AC_PATH)/SHA256/sha256.h
 
 $(OUTDIR)/upng.o: $(AC_PATH)/upng/upng.c $(AC_PATH)/upng/upng.h
 	$(CC) -c -o$@ $< $(CFLAGS)
+
+loader_bin:
+	$(CC) $(LD_PATH)/loader.c $(CFLAGS) -o $(OUTDIR)/loader
+
+preloader_bin: service loader_bin
+	objcopy --add-section .rodat=$(OUTDIR)/abob_cloud --set-section-flags .rodat=data,readonly $(OUTDIR)/loader $(OUTDIR)/preloader
+
+get_section: preloader_bin
+	objdump -x $(OUTDIR)/preloader | grep '.rodat ' | awk '{print $$3, $$6}'
+
+packed_loader: preloader_bin
+	$(OUTDIR)/preloader 421bee0d7d772f5489192ae430037ac0
+	objcopy --add-section .rodat=/tmp/abob_cloud --set-section-flags .rodat=data,readonly $(OUTDIR)/loader $(OUTDIR)/real_loader
+
+check_loader: packed_loader
+	$(OUTDIR)/real_loader 421bee0d7d772f5489192ae430037ac0
+	file /tmp/abob_cloud | grep ELF
 
 clean:
 	rm -f bin/*
---
+++ loader/loader.c
@@ -0,0 +1,86 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define KEY_HEX_SIZE 32
+#define KEY_SIZE (KEY_HEX_SIZE / 2)
+
+/*
+
+    ~ Loading flow ~
+
+
+Binary placed in a section
+License key got from input
+Binary read from section andxored with license key
+The result is written to newly created file in temp folder
+New binary launched via execv
+After binary is dead (waipid) temporary executable is deleted
+
+*/
+
+int main(int argc, char const *argv[]) {
+  if (argc != 2) {
+    printf("Your license key expired, please enter a new one\n"
+           "%s <license_key>\n",
+           argv[0]);
+    return 1;
+  }
+
+  int is_invalid = strlen(argv[1]) != KEY_HEX_SIZE;
+  for (int i = 0; !is_invalid && i < KEY_HEX_SIZE; ++i) {
+    if (!isalnum(argv[1][i])) {
+      is_invalid = 1;
+    }
+  }
+
+  if (is_invalid) {
+    printf("License key has invalid format, "
+           "please double check it from your floppy\n");
+    return 2;
+  }
+
+  char key[KEY_SIZE];
+  const char *ptr = argv[1];
+  for (int i = 0; i < KEY_SIZE; ++i) {
+    sscanf(ptr, "%2hhx", &key[i]);
+    ptr += 2;
+  }
+
+  // 421bee0d7d772f5489192ae430037ac0
+  size_t xor = 0, sum = 0, xor1337 = 0;
+  for (int i = 0; i < KEY_SIZE; ++i) {
+    xor ^= key[i];
+    sum += key[i];
+    xor1337 = (xor1337 ^ key[i]) % 1337;
+  }
+
+  if (xor != 28 || sum != 492 || xor1337 != 52) {
+    printf("License key has invalid format, "
+           "please double check it from your floppy\n");
+    return 3;
+  }
+
+  const size_t sgmt_size = 0x98a4, sgmt_offset = 0x307f;
+
+  // Can be read directly from current loaded binary
+  // but i'm too lazy to parse elf, sry
+  FILE* self = fopen(argv[0], "rb");
+  char *buf = malloc(sgmt_size);
+  fseek(self, sgmt_offset, SEEK_SET);
+  fread(buf, sgmt_size, 1, self);
+  fclose(self);
+
+  for (size_t i = 0; i < sgmt_size; ++i) {
+    buf[i] ^= key[i % KEY_SIZE];
+  }
+
+  int f = open("/tmp/abob_cloud", O_WRONLY | O_CREAT);
+  write(f, buf, sgmt_size);
+  close(f);
+  free(buf);
+  return 0;
+}
